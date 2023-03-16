05712ed7ba8df7e92cab726ccfbcd3d6b6b9a20a
AhnChaeHyeok <88471313+theresia0168@users.noreply.github.com>
2023-03-16 17:45:56 +0900

    Update forensics_64.sh

--- Linux/AhnChaeHyeok/64bit/forensics_64.sh
+++ Linux/AhnChaeHyeok/64bit/forensics_64.sh
@@ -1,50 +1,55 @@
 #!/bin/bash
 
+chmod +x auto_64.sh
+chmod +x logon_64.sh
+chmod +x network_64.sh
+chmod +x proc_64.sh
+chmod +x sysinfo_64.sh
+
 # 포렌식 하고자 하는 데이터 종류 선택
 PS3='Please Select the section You want to Forensic : '
 
-select section in "Autorun" "Logon user" "Network" "Processor" "System Info" "Exit"
+select section in "Autorun" "Logon user" "Network" "Processor" "System Info" "Run all steps" "Exit"
 do
 	if [ "$section" = "Autorun" ];then		# 자동실행 항목
 		echo "Running forensic for $section"
-		chmod +x auto_64.sh
 		./auto_64.sh
 		date >> hash.txt
-		echo    >> hash.txt
 	fi
 	
 	if [ "$section" = "Logon user" ];then		# 로그온 정보
 		echo "Running forensic for $section"
-		chmod +x logon_64.sh
 		./logon_64.sh
 		date >> hash.txt
-		echo    >> hash.txt
 	fi
 	
 	if [ "$section" = "Network" ];then		# 네트워크 정보
 		echo "Running forensic for $section"
-		chmod +x network_64.sh
 		./network_64.sh
 		date >> hash.txt
-		echo    >> hash.txt
 	fi
 	
 	if [ "$section" = "Processor" ];then		# 프로세서 정보
 		echo "Running forensic for $section"
-		chmod +x proc_64.sh
 		./proc_64.sh
 		date >> hash.txt
-		echo    >> hash.txt
 	fi
 	
 	if [ "$section" = "System Info" ];then		# 시스템 정보
 		echo "Running forensic for $section"
-		chmod +x sysinfo_64.sh
 		./sysinfo_64.sh
 		date >> hash.txt
-		echo    >> hash.txt
 	fi
 	
+	if [ "$section" = "Run all steps" ];then
+		echo "Run all steps"
+		./auto_64.sh
+		./logon_64.sh
+		./network_64.sh
+		./proc_64.sh
+		./sysinfo_64.sh
+		date >> hash.txt
+		
 	if [ "$section" = "Exit" ];then			# 프로그램 종료
 		echo Exit the Forensic Program
 		break
