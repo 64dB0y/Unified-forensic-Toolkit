b5e6547a598d734ae91c990d3cca7770a7344638
AhnChaeHyeok <88471313+theresia0168@users.noreply.github.com>
2023-03-16 15:35:55 +0900

    Update proc.sh

--- Linux/AhnChaeHyeok/proc.sh
+++ Linux/AhnChaeHyeok/proc.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 echo Collecting Information of Acting Process
-ps aux >> proc_info.txt
-lsof -i -n >> proc_info.txt
+ps aux >> proc_info.txt         # 현재 시스템에서 실행 중인 프로세스 정보
+lsof -i -n >> proc_info.txt     # 소켓 프로세스 ID, 이름
 
+# 결과물 해시 값 저장
 echo Process information hash >> hash.txt
 ./hash/hash.exe proc_info.txt >> hash.txt
