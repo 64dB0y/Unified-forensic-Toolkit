7db6dd1a955ee1b3aa235de1deeec14c393c129a
AhnChaeHyeok <88471313+theresia0168@users.noreply.github.com>
2023-03-16 16:39:44 +0900

    Update and rename bit.sh to Running.sh

--- Linux/AhnChaeHyeok/Running.sh
+++ Linux/AhnChaeHyeok/Running.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-# 운영체제 x86bit를 확인하는 스크립트
+# 운영체제 x86bit를 확인하여 스크립트를 실행
 # 32bit, 64bit에 각각 사용할 스크립트를 분리해 pc의 bit에 맞는 스크립트가 실행될 수 있도록 만들어야 함
 
 ARCH=$(getconf LONG_BIT)
 
 if [ "$ARCH" = "64" ]; then
-	echo 64bit
+	echo Run 64bit Version
+	chmod +x ./forensics_64.sh
+	./forensics_64.sh
 else
-	echo 32bit
+	echo Run 32bit Version
+	chmod +x ./forensics_32.sh
+	./forensics_32.sh
 fi
