53466f5aa7ad8f1adce5c0caea98a1052f025df3
AhnChaeHyeok <88471313+theresia0168@users.noreply.github.com>
2023-03-16 16:18:15 +0900

    Update forensics.sh

--- Linux/AhnChaeHyeok/forensics.sh
+++ Linux/AhnChaeHyeok/forensics.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+# 해당 기기의 x86 bit 확인
 uname -m
 
+# 포렌식 하고자 하는 데이터 종류 선택
 PS3='Please Select the section You want to Forensic : '
 
 select section in "Autorun" "Logon user" "Network" "Processor" "System Info" "Exit"
 do
-	if [ "$section" = "Autorun" ];then
+	if [ "$section" = "Autorun" ];then		# 자동실행 항목
 		echo "Running forensic for $section"
 		chmod +x auto.sh
 		./auto.sh
@@ -14,7 +16,7 @@ do
 		echo    >> hash.txt
 	fi
 	
-	if [ "$section" = "Logon user" ];then
+	if [ "$section" = "Logon user" ];then		# 로그온 정보
 		echo "Running forensic for $section"
 		chmod +x logon.sh
 		./logon.sh
@@ -22,7 +24,7 @@ do
 		echo    >> hash.txt
 	fi
 	
-	if [ "$section" = "Network" ];then
+	if [ "$section" = "Network" ];then		# 네트워크 정보
 		echo "Running forensic for $section"
 		chmod +x network.sh
 		./network.sh
@@ -30,7 +32,7 @@ do
 		echo    >> hash.txt
 	fi
 	
-	if [ "$section" = "Processor" ];then
+	if [ "$section" = "Processor" ];then		# 프로세서 정보
 		echo "Running forensic for $section"
 		chmod +x proc.sh
 		./proc.sh
@@ -38,7 +40,7 @@ do
 		echo    >> hash.txt
 	fi
 	
-	if [ "$section" = "System Info" ];then
+	if [ "$section" = "System Info" ];then		# 시스템 정보
 		echo "Running forensic for $section"
 		chmod +x sysinfo.sh
 		./sysinfo.sh
@@ -46,7 +48,7 @@ do
 		echo    >> hash.txt
 	fi
 	
-	if [ "$section" = "Exit" ];then
+	if [ "$section" = "Exit" ];then			# 프로그램 종료
 		echo Exit the Forensic Program
 		break
 	fi
