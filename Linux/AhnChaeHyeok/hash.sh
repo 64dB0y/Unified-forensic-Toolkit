377a45257d30419285527511b2af43f78b728b43
AhnChaeHyeok <88471313+theresia0168@users.noreply.github.com>
2023-03-16 15:27:20 +0900

    Delete hash.sh

--- Linux/AhnChaeHyeok/hash.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-./hash.exe ../ARP.txt >> hash.txt
-./hash.exe ../auto.txt >> hash.txt
-./hash.exe ../network_info.txt >> hash.txt
-./hash.exe ../proc_info.txt >> hash.txt
-./hash.exe ../sys_info.txt >> hash.txt
-./hash.exe ../user_info.txt >> hash.txt
