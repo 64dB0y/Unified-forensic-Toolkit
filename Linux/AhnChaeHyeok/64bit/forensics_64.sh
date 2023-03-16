2311a98ad8d75c4d03253a2a24fb60b89ffab0e6
AhnChaeHyeok <88471313+theresia0168@users.noreply.github.com>
2023-03-16 16:41:05 +0900

    Update and rename Linux/AhnChaeHyeok/forensics.sh to Linux/AhnChaeHyeok/64bit/forensics_64.sh

--- Linux/AhnChaeHyeok/64bit/forensics_64.sh
+++ Linux/AhnChaeHyeok/64bit/forensics_64.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# 해당 기기의 x86 bit 확인
-uname -m
-
 # 포렌식 하고자 하는 데이터 종류 선택
 PS3='Please Select the section You want to Forensic : '
 
@@ -10,40 +7,40 @@ select section in "Autorun" "Logon user" "Network" "Processor" "System Info" "Ex
 do
 	if [ "$section" = "Autorun" ];then		# 자동실행 항목
 		echo "Running forensic for $section"
-		chmod +x auto.sh
-		./auto.sh
+		chmod +x auto_64.sh
+		./auto_64.sh
 		date >> hash.txt
 		echo    >> hash.txt
 	fi
 	
 	if [ "$section" = "Logon user" ];then		# 로그온 정보
 		echo "Running forensic for $section"
-		chmod +x logon.sh
-		./logon.sh
+		chmod +x logon_64.sh
+		./logon_64.sh
 		date >> hash.txt
 		echo    >> hash.txt
 	fi
 	
 	if [ "$section" = "Network" ];then		# 네트워크 정보
 		echo "Running forensic for $section"
-		chmod +x network.sh
-		./network.sh
+		chmod +x network_64.sh
+		./network_64.sh
 		date >> hash.txt
 		echo    >> hash.txt
 	fi
 	
 	if [ "$section" = "Processor" ];then		# 프로세서 정보
 		echo "Running forensic for $section"
-		chmod +x proc.sh
-		./proc.sh
+		chmod +x proc_64.sh
+		./proc_64.sh
 		date >> hash.txt
 		echo    >> hash.txt
 	fi
 	
 	if [ "$section" = "System Info" ];then		# 시스템 정보
 		echo "Running forensic for $section"
-		chmod +x sysinfo.sh
-		./sysinfo.sh
+		chmod +x sysinfo_64.sh
+		./sysinfo_64.sh
 		date >> hash.txt
 		echo    >> hash.txt
 	fi
