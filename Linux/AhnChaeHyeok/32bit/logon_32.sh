5683229ad7c9f8aa135d83fe5a2bf6e6097e7590
AhnChaeHyeok <88471313+theresia0168@users.noreply.github.com>
2023-03-16 16:42:32 +0900

    Create logon_32.sh

---
+++ Linux/AhnChaeHyeok/32bit/logon_32.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#로그인 유저 정보
+echo Login user information
+who >> user_info.txt
+
+#현재 로그인 중인 유저
+echo User logged in now
+users >> user_info.txt
+
+#현재 로그인 중인 유저와 실행중인 프로그램
+echo Logged in User and What progress they ar doing
+w >> user_info.txt
+
+#로그인 기록
+echo Login history 
+last >> user_info.txt
+
+#마지막 로그인 기록
+echo Last Login history
+lastlog >> user_info.txt
+
+#결과물에 대한 해시 값
+echo user information hash >> hash.txt
+./hash/hash.exe user_info.txt >> hash.txt
