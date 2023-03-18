54ef8c54713374aa59e03434cfc5d6996aae3d02
AhnChaeHyeok <88471313+theresia0168@users.noreply.github.com>
2023-03-18 17:38:12 +0900

    네트워크 정보

--- Linux/AhnChaeHyeok/64bit/network_64.sh
+++ Linux/AhnChaeHyeok/64bit/network_64.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 
-echo Network Information
-ifconfig >> network_info.txt
-lsof -i -n >> network_info.txt
-netstat -tuln >> network_info.txt
-echo Route Cache
-route -n >> network_info.txt
+mkdir Network
+cd Network
+mkdir hash
+cd ..
 
-echo ARP Cache
+echo Collecting Network Information...
+
+echo ifconfig >> Network/ifconfig.txt
+ifconfig >> Network/ifconfig.txt
+
+echo lsof -i -n >> Network/lsof.txt
+lsof -i -n >> Network/lsof.txt
+
+echo netstat -tuln >> Network/netstat.txt
+netstat -tuln >> Network/netstat.txt
+
+echo Route Cache >> Network/Route.txt
+route -n >> Network/Route.txt
+
+echo ARP Cache >> Network/ARP.txt
 cat /proc/net/arp | while read line; do
 	interface=$(echo "$line" | awk '{print $6}')
 	ip_address=$(echo "$line" | awk '{print $1}')
@@ -17,11 +29,29 @@ cat /proc/net/arp | while read line; do
 	if [ $interface != "interface" ]; then
 		echo "$interface	$ip_address	$hw_address	$arp_type"
 	fi
-done >> network_info.txt
+done >> ARP.txt
 
 echo Network Information Collecting Finished
 
-echo Network information hash >> hash/hash.txt
-./hash.exe network_info.txt >> hash/hash.txt
-date >> hash/hash.txt
-echo    >> hash/hash.txt
+echo ifconfig.txt >> Network/hash.txt
+./hash.exe Network/ifconfig.txt >> Network/hash.txt
+echo    >> Network/hash.txt
+
+echo lsof.txt >> Network/hash.txt
+./hash.exe Network/lsof.txt >> Network/hash.txt
+echo    >> Network/hash.txt
+
+echo netstat.txt >> Network/hash.txt
+./hash.exe Network/netstat.txt >> Network/hash.txt
+echo    >> Network/hash.txt
+
+echo Route.txt >> Network/hash.txt
+./hash.exe Network/Route.txt >> Network/hash.txt
+echo    >> Network/hash.txt
+
+echo ARP.txt >> Network/hash.txt
+./hash.exe Network/ARP.txt >> Network/hash.txt
+echo    >> Network/hash.txt
+
+date >> Network/hash.txt
+echo    >> Network/hash.txt
