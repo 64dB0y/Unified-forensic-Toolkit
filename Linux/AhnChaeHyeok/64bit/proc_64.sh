b408f104b36a7bdbca6195cad65b95137f9496c7
AhnChaeHyeok <88471313+theresia0168@users.noreply.github.com>
2023-03-18 19:50:37 +0900

    프로세스 정보

--- Linux/AhnChaeHyeok/64bit/proc_64.sh
+++ Linux/AhnChaeHyeok/64bit/proc_64.sh
@@ -13,11 +13,11 @@ ps aux >> Process/psaux.txt
 echo lsof >> Process/lsof.txt
 lsof -i -n >> Process/lsof.txt
 
-echo psaux.txt >> Process/hash.txt
-./hash.exe psaux.txt >> Process/hash.txt
-echo    >> Process/hash.txt
+echo psaux.txt >> Process/hash/hash.txt
+./hash.exe psaux.txt >> Process/hash/hash.txt
+echo    >> Process/hash/hash.txt
 
-echo lsof.txt >> Process/hash.txt
-./hash.exe lsof.txt >> Process/hash.txt
-date >> Process/hash.txt
-echo    >> Process/hash.txt
+echo lsof.txt >> Process/hash/hash.txt
+./hash.exe lsof.txt >> Process/hash/hash.txt
+date >> Process/hash/hash.txt
+echo    >> Process/hash/hash.txt
