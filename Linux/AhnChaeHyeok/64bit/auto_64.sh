ac4b75f194d3fbfb005615c8d60d6f65f1fd72b6
AhnChaeHyeok <88471313+theresia0168@users.noreply.github.com>
2023-03-18 17:40:27 +0900

    Update auto_64.sh

--- Linux/AhnChaeHyeok/64bit/auto_64.sh
+++ Linux/AhnChaeHyeok/64bit/auto_64.sh
@@ -5,17 +5,15 @@ cd Autorun
 mkdir hash
 cd ..
 
+echo Collecting Autorun Information...
+
 #List of autorun programs
-echo Collecting autorun programs...
 echo /etc/init.d/ information >> Autorun/auto.txt
 ls -l /etc/init.d/ >> Autorun/auto.txt
 
 #List of autorunning programs
-echo Collecting autorunning programs...
 echo systemctl information >> Autorun/autorunning.txt
 systemctl list-unit-files --type=service --state=enabled >> Autorun/autorunning.txt
-
-echo Autorun program Information Collecting finished
 
 echo auto.txt >> Autorun/hash/hash.txt
 ./hash.exe Autorun/auto.txt >> Autorun/hash/hash.txt
@@ -27,3 +25,5 @@ echo    >> Autorun/hash/hash.txt
 
 date >> Autorun/hash/hash.txt
 echo    >> Autorun/hash/hash.txt
+
+echo Autorun program Information Collecting finished
