3d68e6cb3f9802c625a00fc1ae013be934e2d724
AhnChaeHyeok <88471313+theresia0168@users.noreply.github.com>
2023-03-20 13:53:47 +0900

    Processor Information Collecting Script

--- Linux/Unified/processinfo.bash
+++ Linux/Unified/processinfo.bash
@@ -9,15 +9,15 @@ echo ps aux > Process/psaux.txt
 ps aux >> Process/psaux.txt
 
 # Using top command. CPU usage information
-top -b -n 1 > top_info.txt
+top -b -n 1 > Process/top_info.txt
 
 # Using pstree command
 # pstree 명령어는 리눅스 시스템의 프로세스와 프로세스간의 관계를 트리구조로 보여주는 명령어
-pstree -paul > pstree_info.txt
+pstree -paul > Process/pstree_info.txt
 
 # Using pgrep command
 # 실행중인 프로세스의 ID를 찾는 명령어
-pgrep -a bash > pgrep_info.txt
+pgrep -a bash > Process/pgrep_info.txt
 
 echo lsof > Process/lsof.txt
 lsof -i -n >> Process/lsof.txt
