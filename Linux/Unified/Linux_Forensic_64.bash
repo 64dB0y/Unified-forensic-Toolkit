7c868ddf41d32e068e104ea8e4efe9ebb8e52092
AhnChaeHyeok <88471313+theresia0168@users.noreply.github.com>
2023-03-20 14:04:49 +0900

    Script for running forensic scripts

--- Linux/Unified/Linux_Forensic_64.bash
+++ Linux/Unified/Linux_Forensic_64.bash
@@ -1,41 +1,40 @@
 #!/bin/bash
 
-chmod +x logon.bash
-chmod +x network_64.bash
-chmod +x processinfo.bash
+chmod +x Scripts/*.bash
 
 # Selective statement that selects the area you want to forensics
 PS3='Please Select the section You want to Forensic : '
-select section in "ALL" "Logon Info" "Network Info" "Processor Info"
+select section in "ALL" "Logon Info" "Network Info" "Processor Info" "Exit"
 do
-   	 if [ "$section" = "ALL" ];then              # Forensic ALL section
-		echo "Collecting $section..."
-		./logon.bash
-		./network_64.bash
-		./processinfo.bash
-		echo "Collection Finished"
-		echo
-	fi
+    if [ "$section" = "ALL" ];then              # Forensic ALL section
+        echo "Collecting $section..."
+        for file in Scripts/*.bash
+        do
+            ./"$file"
+        done
+        echo "Collection Finished"
+        echo
+    fi
 
 	if [ "$section" = "Logon Info" ];then       # Forensic Logon section
 		echo "Collecting $section..."
-		./logon.bash
-		echo "Collection Finished"
-		echo
+		./Scripts/logon.bash
+        echo "Collection Finished"
+        echo
 	fi
 	
 	if [ "$section" = "Network" ];then          # Forensic Network section
 		echo "Collecting $section..."
-		./network_64.bash
-		echo "Collection Finished"
-		echo
+		./Scripts/network_64.bash
+        echo "Collection Finished"
+        echo
 	fi
 	
 	if [ "$section" = "Processor Info" ];then   # Forensic Processor section
 		echo "Collecting $section..."
-		./processinfo.bash
-		echo "Collection Finished"
-		echo
+		./Scripts/processinfo.bash
+        echo "Collection Finished"
+        echo
 	fi
 	
 	if [ "$section" = "Exit" ];then             # Exit the program
