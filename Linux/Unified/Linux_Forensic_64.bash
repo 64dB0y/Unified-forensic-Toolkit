cf5b2b83d17ff67869832ceea80802ceb83e4195
AhnChaeHyeok <88471313+theresia0168@users.noreply.github.com>
2023-03-20 13:11:25 +0900

    Add files via upload

--- Linux/Unified/Linux_Forensic_64.bash
+++ Linux/Unified/Linux_Forensic_64.bash
@@ -9,32 +9,36 @@ PS3='Please Select the section You want to Forensic : '
 select section in "ALL" "Logon Info" "Network Info" "Processor Info"
 do
     if [ "$section" = "ALL" ];then              # Forensic ALL section
-        echo "Run forensic for $section"
-        echo
+        echo "Collecting $section..."
         ./logon.bash
         ./network_64.bash
         ./processinfo.bash
+        echo "Collection Finished"
+        echo
 
 	if [ "$section" = "Logon Info" ];then       # Forensic Logon section
-		echo "Run forensic for $section"
-        echo
+		echo "Collecting $section..."
 		./logon.bash
+        echo "Collection Finished"
+        echo
 	fi
 	
 	if [ "$section" = "Network" ];then          # Forensic Network section
-		echo "Run forensic for $section"
-        echo
+		echo "Collecting $section..."
 		./network_64.bash
+        echo "Collection Finished"
+        echo
 	fi
 	
 	if [ "$section" = "Processor Info" ];then   # Forensic Processor section
-		echo "Run forensic for $section"
-        echo
+		echo "Collecting $section..."
 		./processinfo.bash
+        echo "Collection Finished"
+        echo
 	fi
 	
 	if [ "$section" = "Exit" ];then             # Exit the program
-		echo Exit the Forensic Program
+		echo "Exit the Forensic Program"
 		break
 	fi
 	
