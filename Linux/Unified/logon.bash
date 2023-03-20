b7522e691a37e8358c955cf667cd63151b3cf795
AhnChaeHyeok <88471313+theresia0168@users.noreply.github.com>
2023-03-20 13:27:26 +0900

    Add files via upload

--- Linux/Unified/logon.bash
+++ Linux/Unified/logon.bash
@@ -1,8 +1,16 @@
 #!/bin/bash
 # 출력 결과를 logon.txt 파일로 저장
+
+mkdir Logon
+cd Logon
+mkdir hash
+cd ..
+
 exec > logon.txt
 
 # 로그온 사용자 정보 출력 스크립트
+echo Collecting Logon user Information...
+
 echo "---------------------------------------------"
 echo "1. Recently logged in user information"
 lastlog
@@ -22,4 +30,8 @@ users
 echo "---------------------------------------------"
 echo "5. Recently logged out user information"
 last -x | grep down 
+echo "---------------------------------------------"
 
+./hash.exe Logon/logon.txt > Logon/hash/hash.txt
+
+echo Logon user Information Collecting Finished
