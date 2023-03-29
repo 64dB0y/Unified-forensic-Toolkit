ce8ce6f3c5b0432c3e259f91622b1daf3a5f6157
AhnChaeHyeok <88471313+theresia0168@users.noreply.github.com>
2023-03-29 15:03:50 +0900

    Forensic Running Script

--- Linux/Unified/Linux_Forensic_64.bash
+++ Linux/Unified/Linux_Forensic_64.bash
@@ -24,42 +24,42 @@ chmod 777 hash.exe
 PS3='Please Select the section You want to Forensic : '
 select section in "ALL" "Logon Info" "Network Info" "Processor Info" "Exit"
 do
-    if [ "$section" = "ALL" ];then              # Forensic ALL section
-        echo "Collecting $section..."
-        for file in Scripts/*.bash
-        do
-            ./"$file"
-        done
-        echo "Collection Finished"
-        echo
-    fi
+	if [ "$section" = "ALL" ];then              # Forensic ALL section
+		echo "Collecting $section..."
+		for file in Scripts/*.bash
+		do
+		    ./"$file"
+		done
+		echo "Collection Finished"
+		echo
+	fi
 
 	if [ "$section" = "Logon Info" ];then       # Forensic Logon section
 		echo "Collecting $section..."
 		./Scripts/logon.bash
-        echo "Collection Finished"
-        echo
+		echo "Collection Finished"
+		echo
 	fi
-	
+
 	if [ "$section" = "Network" ];then          # Forensic Network section
 		echo "Collecting $section..."
 		./Scripts/network_64.bash
-        echo "Collection Finished"
-        echo
+		echo "Collection Finished"
+		echo
 	fi
-	
+
 	if [ "$section" = "Processor Info" ];then   # Forensic Processor section
 		echo "Collecting $section..."
 		./Scripts/processinfo.bash
-        echo "Collection Finished"
-        echo
+		echo "Collection Finished"
+		echo
 	fi
-	
+
 	if [ "$section" = "Exit" ];then             # Exit the program
 		echo "Exit the Forensic Program"
 		break
 	fi
-	
+
 	echo  
 	continue
 done
