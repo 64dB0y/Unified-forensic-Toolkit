5bd81edbf374b6cfe290cfccb8d1b7659c5ccefa
AhnChaeHyeok <88471313+theresia0168@users.noreply.github.com>
2023-05-08 16:53:42 +0900

    Add files via upload

--- Linux/Unified/Linux_Forensic_64.bash
+++ Linux/Unified/Linux_Forensic_64.bash
@@ -2,7 +2,7 @@
 
 echo "Linux_Forensic_64.bash for forensics in a Linux 64-bit environment"
 echo "by Team_Bocchi_The_Forensic"
-echo "Version 0.02b"
+echo "Version 0.03b"
 echo
 
 # Create a Timestamp
@@ -29,68 +29,148 @@ echo
 chmod +x Scripts/*.bash
 chmod 777 hash.exe
 
-# Selective statement that selects the area you want to forensics
-PS3='Please Select the section You want to Forensic : '
-select section in "ALL" "Logon Info" "Network Info" "Processor Info" "Autorun" "System Info" "Exit"
+PS3='Please Select the section You Want to Forensic : '
+select section in "ALL" "Active Data" "Inactive Data" "Exit"
 do
-	if [ "$section" = "ALL" ];then              # Forensic ALL section
-		echo "Collecting $section..."
-		echo
-		for file in Scripts/*.bash
+	if [ "$section" = "ALL" ];then
+		for file in Inactive_Scripts/*.bash
+			do
+				echo "-------------------------------------------------------------" >> ../mnt2/Forensic_Info.txt
+				./"$file"
+				echo "-------------------------------------------------------------"
+			done
+		
+		for file in Active_Scripts/*.bash
+			do
+				echo "-------------------------------------------------------------" >> ../mnt2/Forensic_Info.txt
+				./"$file"
+				echo "-------------------------------------------------------------"
+			done
+	fi
+	
+	if [ "$section" = "Inactive Data" ];then
+		PS3='Please Select the section You want to Forensic in Inactive Data : '
+		select intact_section in "ALL" "Metadata Info" "Log File Info" "System/Login Info" "Trash/Recycle bin Info" "Web History Info" "Exit"
 		do
-			echo "-------------------------------------------------------------" >> ../mnt2/Forensic_Info.txt
-			./"$file"
-			echo "-------------------------------------------------------------"
+			if [ "$inact_section" = "ALL" ];then
+				echo "Collection $inact_section..."
+				echo
+				for file in Inactive_Scripts/*.bash
+				do
+					echo "-------------------------------------------------------------" >> ../mnt2/Forensic_Info.txt
+					./"$file"
+					echo "-------------------------------------------------------------"
+				done
+				echo "Collection Finished"
+			fi
+
+			if [ "$inact_section" = "Metadata Info" ];then       # Forensic Metadata section
+				echo "Collecting $inact_section..."
+				echo
+				echo "-------------------------------------------------------------" >> ../mnt2/Forensic_Info.txt
+				./Scripts/a_metadata.bash
+				echo "Collection Finished"
+			fi
+
+			if [ "$inact_section" = "Log File Info" ];then       # Forensic Log File section
+				echo "Collecting $inact_section..."
+				echo
+				echo "-------------------------------------------------------------" >> ../mnt2/Forensic_Info.txt
+				./Scripts/b_log.bash
+				echo "Collection Finished"
+			fi
+
+			if [ "$inact_section" = "System/Login Info" ];then       # Forensic System & Login Information section
+				echo "Collecting $inact_section..."
+				echo
+				echo "-------------------------------------------------------------" >> ../mnt2/Forensic_Info.txt
+				./Scripts/c_systeminfo.bash
+				echo "Collection Finished"
+			fi
+
+			if [ "$inact_section" = "Trash/Recycle bin Info" ];then       # Forensic Trash File section
+				echo "Collecting $inact_section..."
+				echo
+				echo "-------------------------------------------------------------" >> ../mnt2/Forensic_Info.txt
+				./Scripts/c_trash.bash
+				echo "Collection Finished"
+			fi
+
+			if [ "$inact_section" = "Web History Info" ];then       # Forensic Web History File section
+				echo "Collecting $inact_section..."
+				echo
+				echo "-------------------------------------------------------------" >> ../mnt2/Forensic_Info.txt
+				./Scripts/d_webhistory.bash
+				echo "Collection Finished"
+			fi
 		done
-		echo "Collection Finished"
 	fi
 
-	if [ "$section" = "Logon Info" ];then       # Forensic Logon section
-		echo "Collecting $section..."
-		echo
-		echo "-------------------------------------------------------------" >> ../mnt2/Forensic_Info.txt
-		./Scripts/logon.bash
-		echo "Collection Finished"
+	if [ "$section" = "Active Data" ];then
+		# Selective statement that selects the area you want to forensics
+		PS3='Please Select the section You want to Forensic in Active Data : '
+		select act_section in "ALL" "Logon Info" "Network Info" "Processor Info" "Autorun" "System Info" "Exit"
+		do
+			if [ "$act_section" = "ALL" ];then              # Forensic ALL section
+				echo "Collecting $act_section..."
+				echo
+				for file in Active_Scripts/*.bash
+				do
+					echo "-------------------------------------------------------------" >> ../mnt2/Forensic_Info.txt
+					./"$file"
+					echo "-------------------------------------------------------------"
+				done
+				echo "Collection Finished"
+			fi
+
+			if [ "$act_section" = "Logon Info" ];then       # Forensic Logon section
+				echo "Collecting $act_section..."
+				echo
+				echo "-------------------------------------------------------------" >> ../mnt2/Forensic_Info.txt
+				./Scripts/logon.bash
+				echo "Collection Finished"
+			fi
+		
+			if [ "$act_section" = "Network" ];then          # Forensic Network section
+				echo "Collecting $act_section..."
+				echo
+				echo "-------------------------------------------------------------" >> ../mnt2/Forensic_Info.txt
+				./Scripts/network_64.bash
+				echo "Collection Finished"
+			fi
+			
+			if [ "$act_section" = "Processor Info" ];then   # Forensic Processor section
+				echo "Collecting $act_section..."
+				echo
+				echo "-------------------------------------------------------------" >> ../mnt2/Forensic_Info.txt
+				./Scripts/processinfo.bash
+				echo "Collection Finished"
+			fi
+			
+			if [ "$act_section" = "Autorun" ];then          # Forensic Network section
+				echo "Collecting $act_section..."
+				echo
+				echo "-------------------------------------------------------------" >> ../mnt2/Forensic_Info.txt
+				./Scripts/auto_64.bash
+				echo "Collection Finished"
+			fi
+		
+			if [ "$act_section" = "System Info" ];then          # Forensic Network section
+				echo "Collecting $act_section..."
+				echo
+				echo "-------------------------------------------------------------" >> ../mnt2/Forensic_Info.txt
+				./Scripts/sysinfo_64.bash
+				echo "Collection Finished"
+			fi
+			
+			if [ "$act_section" = "Exit" ];then             # Exit the program
+				echo "Exit the Forensic Program"
+				echo
+				break
+			fi
+			
+			echo
+			continue
+		done
 	fi
-	
-	if [ "$section" = "Network" ];then          # Forensic Network section
-		echo "Collecting $section..."
-		echo
-		echo "-------------------------------------------------------------" >> ../mnt2/Forensic_Info.txt
-		./Scripts/network_64.bash
-		echo "Collection Finished"
-	fi
-	
-	if [ "$section" = "Processor Info" ];then   # Forensic Processor section
-		echo "Collecting $section..."
-		echo
-		echo "-------------------------------------------------------------" >> ../mnt2/Forensic_Info.txt
-		./Scripts/processinfo.bash
-		echo "Collection Finished"
-	fi
-	
-	if [ "$section" = "Autorun" ];then          # Forensic Network section
-		echo "Collecting $section..."
-		echo
-		echo "-------------------------------------------------------------" >> ../mnt2/Forensic_Info.txt
-		./Scripts/auto_64.bash
-		echo "Collection Finished"
-	fi
-	
-	if [ "$section" = "System Info" ];then          # Forensic Network section
-		echo "Collecting $section..."
-		echo
-		echo "-------------------------------------------------------------" >> ../mnt2/Forensic_Info.txt
-		./Scripts/sysinfo_64.bash
-		echo "Collection Finished"
-	fi
-	
-	if [ "$section" = "Exit" ];then             # Exit the program
-		echo "Exit the Forensic Program"
-		echo
-		break
-	fi
-	
-	echo
-	continue
 done
