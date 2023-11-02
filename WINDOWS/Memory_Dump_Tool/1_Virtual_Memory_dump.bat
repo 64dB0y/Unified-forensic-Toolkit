0b66f001461e9c50fc6159904291c5c56b2b94f1
X3syguy <NexusX4444@gmail.com>
2023-11-02 11:15:44 +0900

    edited virtual memory dump script

--- WINDOWS/Memory_Dump_Tool/1_Virtual_Memory_dump.bat
+++ WINDOWS/Memory_Dump_Tool/1_Virtual_Memory_dump.bat
@@ -3,7 +3,7 @@ setlocal enabledelayedexpansion
 
 set "sysinternals=%~1"
 set "Virtual_Memory_dir=%~2"
-set "TimeStamp=%~3"
+set "_TimeStamp=%~3"
 set "hash=%~4"
 
 echo Choose dump type:
@@ -23,50 +23,45 @@ if "%PROCESSOR_ARCHITECTURE%"=="AMD64" (
 for /f "tokens=2" %%a in ('tasklist ^| findstr /r /b "[A-Za-z]"') do (
     set "timestamp=!date! !time!"
     if %dumpType%==1 (
-        echo [%timestamp%] Starting procdump for process: %%a >> "%Virtual_Memory_dir%\full_memory\timestamp\%%a_timestamp.txt"
+        echo [%_TimeStamp%] Starting procdump for process: %%a >> %_TimeStamp%
         echo Creating full memory dump for process: %%a
         if not exist "%Virtual_Memory_dir%\full_memory" mkdir "%Virtual_Memory_dir%\full_memory"
         if not exist "%Virtual_Memory_dir%\full_memory\hash" mkdir "%Virtual_Memory_dir%\full_memory\hash"
-        if not exist "%Virtual_Memory_dir%\full_memory\timestamp" mkdir "%Virtual_Memory_dir%\full_memory\timestamp"
         "!sysinternals!\%procdump%" -ma "%%a" "%Virtual_Memory_dir%\full_memory\%%a_memory_dump.dmp"
-        echo [%timestamp%] Finished procdump for process: %%a >> "%Virtual_Memory_dir%\full_memory\timestamp\%%a_timestamp.txt"
+        echo [!timestamp!] Finished procdump for process: %%a >> %_TimeStamp%
         echo Calculating hash for process: %%a
         "!hash!\%hashdeep%" "%Virtual_Memory_dir%\full_memory\%%a_memory_dump.dmp" > "%Virtual_Memory_dir%\full_memory\hash\%%a_memory_dump_hash.txt"
-        echo [%timestamp%] HASH for process %%a >> "%Virtual_Memory_dir%\full_memory\timestamp\%%a_timestamp.txt"
+        echo [!timestamp!] HASH for process %%a >> %_TimeStamp%
     ) else if %dumpType%==2 (
-        echo [%timestamp%] Starting procdump for process: %%a >> "%Virtual_Memory_dir%\kernel_memory\timestamp\%%a_timestamp.txt"
+        echo [!timestamp!] Starting procdump for process: %%a >> %_TimeStamp%
         echo Creating kernel memory dump for process: %%a
         if not exist "%Virtual_Memory_dir%\kernel_memory" mkdir "%Virtual_Memory_dir%\kernel_memory"
         if not exist "%Virtual_Memory_dir%\kernel_memory\hash" mkdir "%Virtual_Memory_dir%\kernel_memory\hash"
-        if not exist "%Virtual_Memory_dir%\kernel_memory\timestamp" mkdir "%Virtual_Memory_dir%\kernel_memory\timestamp"
         "!sysinternals!\%procdump%" -mk "%%a" "%Virtual_Memory_dir%\kernel_memory\%%a_kernel_memory_dump.dmp"
-        echo [%timestamp%] Finished procdump for process: %%a >> "%Virtual_Memory_dir%\kernel_memory\timestamp\%%a_timestamp.txt"
+        echo [!timestamp!] Finished procdump for process: %%a >> %_TimeStamp%
         echo Calculating hash for process: %%a
         "!hash!\%hashdeep%" "%Virtual_Memory_dir%\kernel_memory\%%a_kernel_memory_dump.dmp" > "%Virtual_Memory_dir%\kernel_memory\hash\%%a_kernel_memory_dump_hash.txt"
-        echo [%timestamp%] HASH for process %%a >> "%Virtual_Memory_dir%\kernel_memory\timestamp\%%a_timestamp.txt"
+        echo [!timestamp!] HASH for process %%a >> %_TimeStamp%
     ) else if %dumpType%==3 (
-        echo [%timestamp%] Starting procdump for process: %%a >> "%Virtual_Memory_dir%\full_memory\timestamp\%%a_timestamp.txt"
+        echo [!timestamp!] Starting procdump for process: %%a >> %_TimeStamp%
         echo Creating full memory dump for process: %%a
         if not exist "%Virtual_Memory_dir%\full_memory" mkdir "%Virtual_Memory_dir%\full_memory"
         if not exist "%Virtual_Memory_dir%\full_memory\hash" mkdir "%Virtual_Memory_dir%\full_memory\hash"
-        if not exist "%Virtual_Memory_dir%\full_memory\timestamp" mkdir "%Virtual_Memory_dir%\full_memory\timestamp"
         "!sysinternals!\%procdump%" -ma "%%a" "%Virtual_Memory_dir%\full_memory\%%a_memory_dump.dmp"
-        echo [%timestamp%] Finished procdump for process: %%a >> "%Virtual_Memory_dir%\full_memory\timestamp\%%a_timestamp.txt"
+        echo [!timestamp!] Finished procdump for process: %%a >> %_TimeStamp%
         echo Calculating hash for process: %%a
         "!hash!\%hashdeep%" "%Virtual_Memory_dir%\full_memory\%%a_memory_dump.dmp" > "%Virtual_Memory_dir%\full_memory\hash\%%a_memory_dump_hash.txt"
-        echo [%timestamp%] HASH for process %%a >> "%Virtual_Memory_dir%\full_memory\timestamp\%%a_timestamp.txt"
+        echo [!timestamp!] HASH for process %%a >> %_TimeStamp%
 
-        echo [%timestamp%] Starting procdump for process: %%a >> "%Virtual_Memory_dir%\kernel_memory\timestamp\%%a_timestamp.txt"
+        echo [!timestamp!] Starting procdump for process: %%a >> %_TimeStamp%
         echo Creating kernel memory dump for process: %%a
         if not exist "%Virtual_Memory_dir%\kernel_memory" mkdir "%Virtual_Memory_dir%\kernel_memory"
         if not exist "%Virtual_Memory_dir%\kernel_memory\hash" mkdir "%Virtual_Memory_dir%\kernel_memory\hash"
-        if not exist "%Virtual_Memory_dir%\kernel_memory\timestamp" mkdir "%Virtual_Memory_dir%\kernel_memory\timestamp"
         "!sysinternals!\%procdump%" -mk "%%a" "%Virtual_Memory_dir%\kernel_memory\%%a_kernel_memory_dump.dmp"
-        echo [%timestamp%] Finished procdump for process: %%a >> "%Virtual_Memory_dir%\kernel_memory\timestamp\%%a_timestamp.txt"
+        echo [!timestamp!] Finished procdump for process: %%a >> %_TimeStamp%
         echo Calculating hash for process: %%a
         "!hash!\%hashdeep%" "%Virtual_Memory_dir%\kernel_memory\%%a_kernel_memory_dump.dmp" > "%Virtual_Memory_dir%\kernel_memory\hash\%%a_kernel_memory_dump_hash.txt"
-        echo [%timestamp%] HASH for process %%a >> "%Virtual_Memory_dir%\kernel_memory\timestamp\%%a_timestamp.txt"
+        echo [!timestamp!] HASH for process %%a >> %_TimeStamp%
     )
 )
 pause
-
